c2358008ff4fe386563d0227cfb7257c1ad3b033
meggs1 <megan.schertz@gmail.com>
2021-01-05 19:42:29 -0600

    commit tree e35e608a0eab5517b36318e5204444fac8696bd0

---
+++ db/migrate/005_create_characters.rb
@@ -0,0 +1,11 @@
+class CreateCharacters < ActiveRecord::Migration[5.1]
+
+    def change
+        create_table :characters do |a|
+            a.string :name
+            a.integer :actor_id
+            a.integer :show_id
+        end
+    end
+
+end
